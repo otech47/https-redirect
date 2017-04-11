87e3653aa63dd065118e011cd12b8fc861818c89
Oscar Lafarga <otech47@gmail.com>
2017-04-11 06:59:05 -0400

    added health check

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.use(function(req, res, next) {
     res.redirect('https://www.careerscore.com' + req.path);
 });
 
+app.get('/healthcheck', function(req, res, next) {
+    res.send(200);
+});
+
 app.listen(port, function() {
     console.log('https-redirect: Server running on port ' + port);
 });
